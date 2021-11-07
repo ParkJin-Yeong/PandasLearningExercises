405ff54c0b2bad6fd96996d32c80d98cfe6882c4
Ryan Clark <clark.ryan.l@gmail.com>
2021-11-06 23:31:43 -0700

    lesson 02 commit

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import pandas as pd
 pd.set_option("display.max_columns", 85)
 pd.set_option("display.max_rows", 85)
 
-#df = pd.read_csv("2019/survey_results_public.csv")
+df = pd.read_csv("2019/survey_results_public.csv")
 schema_df = pd.read_csv('2019/survey_results_schema.csv')
 
-print(schema_df)
+# people = {
+#     "first": ["Corey", 'Jane', 'John'],
+#     "last": ["Schafer", 'Doe', 'Doe'],
+#     "email": ["CoreyMSchafer@gmail.com", 'JaneDoe@email.com', 'JohnDoe@email.com']
+# }
+#
+# df = pd.DataFrame(people)
+print(df.loc[0:2, 'Hobbyist':'Employment'])
